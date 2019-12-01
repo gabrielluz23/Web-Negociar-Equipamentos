0dbe5736ddd59d1b138e88c72cb3acf49c8b7993
gabriel yago <gabriellok1212@gmail.com>
2019-12-01 15:22:09 -0300

    Tela de Login  Funcioanando

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,46 +1,47 @@
-import React from 'react';
+import React, {useState} from 'react';
+import api from './services/api';
 
 import './App.css';
+
 import logo from './images/logo.jpg';
+
 function App() {
+const [email,setemail] = useState('');
+const [senha,setsenha] = useState('');
+ async function handleSubimit(event) {
+    event.preventDefault();
+  const response = await  api.post('/login',{email,senha});
+  const {_id} = response.data;
+  localStorage.setItem("id",_id);
+  }
   return (
    <div className="container">
      <img src={logo} alt=""/>
+     
      <div className="content">
        <p>
          Faça <strong> Trocas</strong> e <strong>Vendas</strong> de equipamentos usados, Desapegue.
        </p>
       
-       <form >
-       <label htmlFor="nome">Nome *</label>
-         <input type="nome" 
-         id="nome" 
-         placeholder = "Seu Nome"/>
-
+       <form  onSubmit = {handleSubimit}>
          <label htmlFor="email">E-MAIL *</label>
          <input type="email" 
          id="email" 
-         placeholder = "Seu Email"/>
+         placeholder = "Seu Email"
+         value = {email}
+         onChange = {event =>setemail (event.target.value)}
+         />
 
         <label htmlFor="senha">Senha *</label>
          <input 
          type="password" 
          id="senha" 
-         placeholder = "Sua Senha"/>
+         placeholder = "Sua Senha"
+         value= {senha}
+         onChange = {event =>setsenha (event.target.value)}/>
 
-          <label htmlFor="confsenha">Confirmar Senha *</label>
-         <input 
-         type="password" 
-         id="confsenha" 
-         placeholder = "Confirmar Senha"/>
-
-         <label htmlFor="telefone">Telefone *</label>
-         <input 
-         type="telefone" 
-         id="telefone" 
-         placeholder = "Seu Telefone "/>
-         <button type="submit" className= "btn"> Cadastrar-se </button>
-         <button  className= "btn"> Ja Sou Cadastrado </button>
+         <button type="submit" className= "btn"> Fazer Login</button>
+         <button  className= "btn"> Fazer Cadastro </button>
        </form>
      </div>
    </div>
